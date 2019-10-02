a8085fd520f67e2b6cf2ae2d7a2943aab2199658
Saurabh singh <2908aman@gmail.com>
2019-10-02 09:42:46 +0530

    edit comments

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import Shop from './containers/Shop';
 class App extends Component {
+  // props is for taking data from the parent
   constructor(props) {
     super(props);
     this.state = {  };
   }
+  // render call first
   render() {
+    
+    // return print the component or return the component
     return (
      <Shop/>
     );
   }
 }
-
+// default export
 export default App;
